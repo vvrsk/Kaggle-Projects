360980554477184dc97d15d7caca10156c2cfff0
Karthik <karthikvvrs@gmail.com>
2017-05-05 23:47:13 -0500

    Commit #4

---
+++ Kaggle_2sigma/setup.R
@@ -0,0 +1,25 @@
+# Load for Text Mining
+#Change with the Wordcloud2for respective shape.  https://cran.r-project.org/web/packages/wordcloud2/vignettes/wordcloud.html
+library("tm")
+library("wordcloud")
+library("RColorBrewer")
+library(ggplot2)
+library(ggmap)
+library(plotly)
+library(leaflet)
+require(foreign)
+require(nnet)
+require(ggplot2)
+require(reshape2)
+
+
+
+library("tree", lib.loc="~/R/win-library/3.3")
+library("rpart")
+require("party")
+library(rattle)
+
+
+library(chron)
+library(plyr)
+library(ggplot2)
